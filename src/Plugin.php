2470d10d4f1af1fca67a911f28f0081765135f25
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-08 16:53:28 -0300

    Update plugin

---
+++ src/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * Date: 08/06/2018
+ * Time: 16:48
+ */
+
+namespace Settings;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @author Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
+ *
+ * @package Settings
+ */
+class Plugin extends BasePlugin
+{
+
+}
